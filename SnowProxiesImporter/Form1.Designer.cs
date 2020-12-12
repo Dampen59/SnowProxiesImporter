f63780d77e43bbf6946a791a34dab5867e0094e9
Dampen59 <Dampen59@free.fr>
2020-12-12 21:00:49 +0100

    Init

---
+++ SnowProxiesImporter/Form1.Designer.cs
@@ -0,0 +1,283 @@
+﻿namespace SnowProxiesImporter
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.lbl_statePC = new System.Windows.Forms.Label();
+            this.lbl_stateTouch = new System.Windows.Forms.Label();
+            this.lbl_stateRetro = new System.Windows.Forms.Label();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.btn_selectProxies = new System.Windows.Forms.Button();
+            this.proxiesListDialog = new System.Windows.Forms.OpenFileDialog();
+            this.button1 = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.radioButton3);
+            this.groupBox1.Controls.Add(this.radioButton2);
+            this.groupBox1.Controls.Add(this.radioButton1);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 129);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(741, 107);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 16);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(435, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Veuillez sélectionner l\'édition de SnowBot pour laquelle vous souhaitez importer " +
+    "vos proxies";
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(9, 32);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(85, 17);
+            this.radioButton1.TabIndex = 1;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "SnowBot PC";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            this.radioButton1.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(9, 55);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(102, 17);
+            this.radioButton2.TabIndex = 2;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "SnowBot Touch";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            this.radioButton2.CheckedChanged += new System.EventHandler(this.radioButton2_CheckedChanged);
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Location = new System.Drawing.Point(9, 78);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(97, 17);
+            this.radioButton3.TabIndex = 3;
+            this.radioButton3.TabStop = true;
+            this.radioButton3.Text = "SnowBot Retro";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            this.radioButton3.CheckedChanged += new System.EventHandler(this.radioButton3_CheckedChanged);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.lbl_stateRetro);
+            this.groupBox2.Controls.Add(this.lbl_stateTouch);
+            this.groupBox2.Controls.Add(this.lbl_statePC);
+            this.groupBox2.Controls.Add(this.label5);
+            this.groupBox2.Controls.Add(this.label4);
+            this.groupBox2.Controls.Add(this.label3);
+            this.groupBox2.Controls.Add(this.label2);
+            this.groupBox2.Location = new System.Drawing.Point(12, 12);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(741, 111);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(6, 16);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(142, 13);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Détection des configurations";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(6, 39);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(76, 13);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "SnowBot PC : ";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(6, 61);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(93, 13);
+            this.label4.TabIndex = 2;
+            this.label4.Text = "SnowBot Touch : ";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(6, 84);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(88, 13);
+            this.label5.TabIndex = 3;
+            this.label5.Text = "SnowBot Retro : ";
+            // 
+            // lbl_statePC
+            // 
+            this.lbl_statePC.AutoSize = true;
+            this.lbl_statePC.Location = new System.Drawing.Point(76, 39);
+            this.lbl_statePC.Name = "lbl_statePC";
+            this.lbl_statePC.Size = new System.Drawing.Size(64, 13);
+            this.lbl_statePC.TabIndex = 4;
+            this.lbl_statePC.Text = "%state_pc%";
+            // 
+            // lbl_stateTouch
+            // 
+            this.lbl_stateTouch.AutoSize = true;
+            this.lbl_stateTouch.Location = new System.Drawing.Point(93, 61);
+            this.lbl_stateTouch.Name = "lbl_stateTouch";
+            this.lbl_stateTouch.Size = new System.Drawing.Size(79, 13);
+            this.lbl_stateTouch.TabIndex = 5;
+            this.lbl_stateTouch.Text = "%state_touch%";
+            // 
+            // lbl_stateRetro
+            // 
+            this.lbl_stateRetro.AutoSize = true;
+            this.lbl_stateRetro.Location = new System.Drawing.Point(87, 84);
+            this.lbl_stateRetro.Name = "lbl_stateRetro";
+            this.lbl_stateRetro.Size = new System.Drawing.Size(73, 13);
+            this.lbl_stateRetro.TabIndex = 6;
+            this.lbl_stateRetro.Text = "%state_retro%";
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.btn_selectProxies);
+            this.groupBox3.Controls.Add(this.label6);
+            this.groupBox3.Location = new System.Drawing.Point(12, 242);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(741, 66);
+            this.groupBox3.TabIndex = 2;
+            this.groupBox3.TabStop = false;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(6, 16);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(421, 13);
+            this.label6.TabIndex = 4;
+            this.label6.Text = "Sélectionnez le fichier contenant votre liste de proxies au format \"ip:port:user:" +
+    "pass:alias\"";
+            // 
+            // btn_selectProxies
+            // 
+            this.btn_selectProxies.Location = new System.Drawing.Point(9, 32);
+            this.btn_selectProxies.Name = "btn_selectProxies";
+            this.btn_selectProxies.Size = new System.Drawing.Size(726, 23);
+            this.btn_selectProxies.TabIndex = 5;
+            this.btn_selectProxies.Text = "Parcourir ...";
+            this.btn_selectProxies.UseVisualStyleBackColor = true;
+            this.btn_selectProxies.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // proxiesListDialog
+            // 
+            this.proxiesListDialog.Filter = "Fichiers Textes|*.txt";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 328);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(741, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Importer mes proxies !";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(765, 363);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "SnowProxiesImporter - Dampen59#4187 - for https://www.snowbot.eu/";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Label lbl_stateRetro;
+        private System.Windows.Forms.Label lbl_stateTouch;
+        private System.Windows.Forms.Label lbl_statePC;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.Button btn_selectProxies;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.OpenFileDialog proxiesListDialog;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ SnowProxiesImporter/Form1.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+
+namespace SnowProxiesImporter
+{
+    public partial class Form1 : Form
+    {
+
+        private const string snowBotDataPath = "\\SwiftBot\\Snowbot.exe_Url_3xl2s2jpuburhzypal2aujljehcxxm54\\1.0.0.0\\user.config";
+        private const string snowBotTouchDataPath = "\\Snowbot_company\\SnowbotTouch.exe_Url_dcl3c1umqia1hgfui4wmtc5w2yyzfwsl\\1.0.0.0\\user.config";
+        private const string snowBotRetroDataPath = "\\SwiftBot\\Snowbot.exe_Url_gt4m3hjyqihhzfktxxcn1dxqdso4b50c\\1.0.0.0\\user.config";
+
+        private string localAppDataPath = Environment.GetEnvironmentVariable("LocalAppData");
+
+        private string pathToImport = String.Empty;
+
+        private int detectedConfigurations = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            if (File.Exists(localAppDataPath + snowBotDataPath))
+            {
+                lbl_statePC.Text = "Configuration détectée";
+                lbl_statePC.ForeColor = Color.Green;
+                detectedConfigurations++;
+            } else {
+                lbl_statePC.Text = "Configuration non disponible";
+                lbl_statePC.ForeColor = Color.Red;
+            }
+
+            if (File.Exists(localAppDataPath + snowBotTouchDataPath))
+            {
+                lbl_stateTouch.Text = "Configuration détectée";
+                lbl_stateTouch.ForeColor = Color.Green;
+                detectedConfigurations++;
+            }
+            else
+            {
+                lbl_stateTouch.Text = "Configuration non disponible";
+                lbl_stateTouch.ForeColor = Color.Red;
+            }
+
+            if (File.Exists(localAppDataPath + snowBotRetroDataPath))
+            {
+                lbl_stateRetro.Text = "Configuration détectée";
+                lbl_stateRetro.ForeColor = Color.Green;
+                detectedConfigurations++;
+            }
+            else
+            {
+                lbl_stateRetro.Text = "Configuration non disponible";
+                lbl_stateRetro.ForeColor = Color.Red;
+            }
+
+            if (detectedConfigurations == 0)
+            {
+                MessageBox.Show("Aucune configuration de SnowBot n'a été trouvée sur votre système." + Environment.NewLine + "Merci de lancer SnowBot au moins une fois et ajouter un proxy (valide ou pas) manuellement.", "Erreur", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                Application.Exit();
+            }
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (proxiesListDialog.ShowDialog() != DialogResult.OK)
+            {
+                MessageBox.Show("Veuillez sélectionner votre liste de proxies !!");
+                return;
+            } else
+            {
+                btn_selectProxies.Text = proxiesListDialog.FileName;
+            }
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (radioButton1.Checked)
+            {
+                pathToImport = localAppDataPath + snowBotDataPath;
+            }
+        }
+
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+            if (radioButton2.Checked)
+            {
+                pathToImport = localAppDataPath + snowBotTouchDataPath;
+            }
+        }
+
+        private void radioButton3_CheckedChanged(object sender, EventArgs e)
+        {
+            if (radioButton3.Checked)
+            {
+                pathToImport = localAppDataPath + snowBotRetroDataPath;
+            }
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            if (btn_selectProxies.Text == "Parcourir ...")
+            {
+                MessageBox.Show("Merci de sélectionner un fichier contenant votre liste de proxies !", "Erreur", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            XmlDocument snowbotConfig = new XmlDocument();
+            snowbotConfig.Load(pathToImport);
+            XmlNode root = snowbotConfig.DocumentElement;
+            XmlNode proxiesNode;
+
+            if (radioButton2.Checked)
+            {
+                proxiesNode = root.SelectSingleNode("userSettings/SwiftBotTouch.My.MySettings/setting[@name='proxies']/value/ArrayOfString");
+            } else
+            {
+                proxiesNode = root.SelectSingleNode("userSettings/SwiftBot.My.MySettings/setting[@name='proxies']/value/ArrayOfString");
+            }
+
+            var proxies = File.ReadLines(btn_selectProxies.Text);
+            foreach (string proxyElement in proxies)
+            {
+                string[] proxyData = proxyElement.Split(':');
+
+                if (proxyData[0] == null | proxyData[1] == null | proxyData[2] == null | proxyData[3] == null | proxyData[4] == null)
+                {
+                    MessageBox.Show("Erreur sur le proxy, merci de vérifier votre fichier");
+                    return;
+                }
+            }
+
+            foreach (string proxyElement in proxies)
+            {
+                string[] proxyData = proxyElement.Split(':');
+                XmlElement elem = snowbotConfig.CreateElement("string");
+                elem.InnerText = proxyData[0] + ":" + proxyData[1] + ":" + proxyData[2] + ":" + proxyData[3] + ":" + proxyData[4];
+                proxiesNode.AppendChild(elem);
+            }
+
+            snowbotConfig.Save(pathToImport);
+
+            MessageBox.Show("Importation des proxy : OK");
+
+        }
+    }
+}
